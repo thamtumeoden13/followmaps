2de8eee85adc397d6e7705291ba5b6d9120b254c
Le Hoang Vu <ThamTuMeoDen13@Les-MacBook-Pro.local>
2020-03-20 16:46:08 +0700

    add mapsgooole

--- App.js
+++ App.js
@@ -6,109 +6,142 @@
  * @flow
  */
 
-import React from 'react';
+import React, { Fragment, Component } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Dimensions,
+    Platform,
+    Alert
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout, Polygon, Circle } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+import { PERMISSIONS, request } from 'react-native-permissions';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+console.disableYellowBox = true
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            markers: [],
+            coordinates: [
+                { name: 'Burger', latitude: 37.8025259, longitude: -122.4351431, },
+                { name: 'Pizza', latitude: 37.7946386, longitude: -122.421646, },
+                { name: 'Soup', latitude: 37.7665248, longitude: -122.4165628, },
+                { name: 'Sushi', latitude: 37.7834153, longitude: -122.4527787, },
+                { name: 'Curry', latitude: 37.7948105, longitude: -122.4596065, },
+            ],
+            // initialPosition: {
+            //     latitude: 37.78825,
+            //     longitude: -122.4324,
+            //     latitudeDelta: 0.09,
+            //     longitudeDelta: 0.035
+            // }
+        }
+    }
+
+    componentDidMount() {
+        this.requestLocationPermission();
+    }
+
+    requestLocationPermission = async () => {
+        if (Platform.OS === 'ios') {
+            const response = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+            console.log('Iphone: ', response)
+            if (response === 'granted') {
+                this.localCurrentPosition()
+            }
+        }
+        else {
+            const response = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+            console.log('Android: ', response)
+            if (response === 'granted') {
+                this.localCurrentPosition()
+            }
+        }
+    }
+
+    localCurrentPosition = () => {
+        Geolocation.getCurrentPosition(
+            position => (console.log(JSON.stringify(position))),
+            // error => Alert.alert(error.message),
+            // { enableHighAccuracy: true, timeout: 10000, maximumAge: 1000 }
+        );
+    }
+
+    render() {
+
+        return (
+            <Fragment>
+                <StatusBar barStyle="dark-content" />
+                <View style={styles.container}>
+                    <MapView
+                        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                        showsUserLocation={true}
+                        // ref={map => this._map = map}
+                        style={styles.map}
+                        region={{
+                            latitude: 37.78825,
+                            longitude: -122.4324,
+                            latitudeDelta: 0.09,
+                            longitudeDelta: 0.035,
+                        }}
+                    // initialRegion={this.state.initialPosition}
+                    >
+                        <Polygon
+                            coordinates={this.state.coordinates}
+                            fillColor={'rgba(100,100,200,0.3)'}
+                            strokeWidth={1}
+                        />
+                        <Circle
+                            center={{ latitude: 37.8025259, longitude: -122.4351431 }}
+                            radius={1000}
+                        />
+                        {
+                            this.state.coordinates.map(marker => (
+                                <Marker
+                                    key={marker.name}
+                                    coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+                                    title={marker.name}
+                                >
+                                    <Callout>
+                                        <Text>{marker.name}</Text>
+                                    </Callout>
+                                </Marker>
+                            ))
+                        }
+
+                    </MapView>
+                </View>
+            </Fragment>
+        );
+    }
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        height: '100%',
+        width: '100%',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
 });
 
 export default App;
